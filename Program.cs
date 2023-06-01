5b5f550a43fb0540c61f2621bcebb85ca4f270ba
Lucas-oliver-marques <lucas.oliveiramarques@sounidesc.com.br>
2023-05-31 21:23:50 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int nota1, nota2, nota3, nota4;
+
+        Console.WriteLine("Digite as quatro notas do aluno (de 1 a 10):");
+
+        Console.Write("Nota 1: ");
+        nota1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Nota 2: ");
+        nota2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Nota 3: ");
+        nota3 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Nota 4: ");
+        nota4 = Convert.ToInt32(Console.ReadLine());
+
+        int media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+        Console.WriteLine("Média: " + media);
+
+        if (media >= 6)
+        {
+            Console.WriteLine("Aprovado");
+        }
+        else
+        {
+            Console.WriteLine("Reprovado");
+        }
+    }
+}
